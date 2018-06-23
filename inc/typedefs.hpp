569864740c89ecd008c331a865acf525d4bccd69
mips1234 <mips.zhu@yahoo.com>
2018-06-23 20:12:04 +0800

    type defines

---
+++ inc/typedefs.hpp
@@ -0,0 +1,17 @@
+#ifndef __TYPEDEFS_HPP__
+#define __TYPEDEFS_HPP_
+
+typedef int int32_t;
+typedef short int16_t;
+typedef signed char int8_t;
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
+
+//TODO: define assert
+#define ASSERT(cond)    
+enum
+{
+	ENTRY_NOT_FOUND = 0xFFFFFFFF
+};
+#endif
